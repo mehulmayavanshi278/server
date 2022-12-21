ce58d49567be3776335bc26283ebb009046a6408
mehulmayavanshi278 <mehulmayavanshi0867@gmail.com>
2022-12-21 15:40:11 +0530

    xyz

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000
 const router = require("./routers/auth");
 
 app.use(require("cors")({
-    origin: 'https://frontendbymehul.onrender.com/'
+    origin: 'https://mehulmayavanshifooddelieveryfrontend.netlify.app'
 }))
 app.use(express.json());
 app.use(cookieparser());
