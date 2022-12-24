ad52c1ade60fd75c9f0e3630ade8022a35999862
mehulmayavanshi278 <mehulmayavanshi0867@gmail.com>
2022-12-24 13:41:25 +0530

    nearbydonee

--- src/routers/middlevare/authenticate.js
+++ src/routers/middlevare/authenticate.js
@@ -1,21 +1,30 @@
 const express  = require("express");
 const jwt = require("jsonwebtoken");
+const cookieparser = require("cookie-parser")
 const suser = require("../../module/susers")
 const authenticate = async(req,res,next)=>{
    try{
-    const token = req.cookies.jwt;
+  
+      const token = req.cookies.jwt;
+      if(token){
+         const verifyuser = jwt.verify(token,process.env.SECRETKEY);
+         if(verifyuser){
+          const rootuser = await suser.findOne({_id:verifyuser._id});
+          req.rootuser = rootuser;
+          req.token = token;
+          req.id = rootuser.id;
+          console.log(req.rootuser);
+          next();
+         }else{
+           return res.status(201).send('token is' + token)
+         }
+      }else{
+         return res.status(201).send("token not found");
+      }
 
-    const verifyuser = jwt.verify(token,process.env.SECRETKEY);
-    if(verifyuser){
-     const rootuser = await suser.findOne({_id:verifyuser.id});
-     req.rootuser = rootuser;
-     req.token = token;
-     req.id = rootuser.id;
-     console.log(req.rootuser);
-     next();
-    }
+ 
 
-   return res.status(201).send("not vailidate");
+ 
    }catch(err){
       return res.status(201).send("not valid");
     console.log(err);
